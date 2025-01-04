dc50593ff057470d2d98effad7a8efcf921af865
fanstrong <kingindoslayer@gmail.com>
2025-01-04 12:44:30 +0700

    Update setup.js

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,40 +1,46 @@
-import _0x324b27 from 'fs';
-import _0x48497d from 'path';
-async function fileExists(_0x4e9692) {
+import fs from "fs";
+import path from "path";
+
+async function fileExists(filePath) {
   try {
-    await _0x324b27.promises.access(_0x4e9692);
+    await fs.promises.access(filePath);
     return true;
   } catch {
     return false;
   }
 }
-async function copyFile(_0x4d07e1, _0x16cd4f) {
+
+async function copyFile(src, dest) {
   try {
-    if (await fileExists(_0x16cd4f)) {
-      console.log("File already exists at " + _0x16cd4f + ", skipping copy.");
+    if (await fileExists(dest)) {
+      console.log(`File already exists at ${dest}, skipping copy.`);
     } else {
-      await _0x324b27.promises.copyFile(_0x4d07e1, _0x16cd4f);
-      console.log("Copied " + _0x4d07e1 + " to " + _0x16cd4f);
+      await fs.promises.copyFile(src, dest);
+      console.log(`Copied ${src} to ${dest}`);
     }
-  } catch (_0x215112) {
-    console.error("Error copying file from " + _0x4d07e1 + " to " + _0x16cd4f + ':', _0x215112);
+  } catch (err) {
+    console.error(`Error copying file from ${src} to ${dest}:`, err);
   }
 }
-const copyOperations = [{
-  'src': _0x48497d.join('config', "config_tmp.js"),
-  'dest': _0x48497d.join("config", 'config.js')
-}, {
-  'src': _0x48497d.join('config', "proxy_list_tmp.js"),
-  'dest': _0x48497d.join("config", "proxy_list.js")
-}];
+
+const copyOperations = [
+  {
+    src: path.join("config", "config_tmp.js"),
+    dest: path.join("config", "config.js"),
+  },
+  {
+    src: path.join("accounts", "accounts_tmp.js"),
+    dest: path.join("accounts", "accounts.js"),
+  },
+];
+
 (async () => {
-  console.log("Copying Template File");
-  for (let {
-    src: _0x142cf4,
-    dest: _0x3dc042
-  } of copyOperations) {
-    await copyFile(_0x142cf4, _0x3dc042);
+  console.log(`Copying Template File`);
+  for (let { src, dest } of copyOperations) {
+    await copyFile(src, dest);
   }
-  console.log("\nSetup Complete");
-  console.log("Open and configure\n- config/config.js\n- config/proxy_list.js\n ");
+  console.log(`\nSetup Complete`);
+  console.log(
+    `Open and configure\n- accounts/accounts.js\n- config/config.js\n `
+  );
 })();
