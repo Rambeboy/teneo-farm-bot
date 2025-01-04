6ec2333f8bbdd4099b49cf408f987c5c8912ad97
fanstrong <kingindoslayer@gmail.com>
2025-01-04 13:06:45 +0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -147,12 +147,12 @@ function displayAccountData(_0x5511c3) {
   console.log(chalk.whiteBright("Message       : " + messages[_0x5511c3]));
   const _0x1696ed = proxies[_0x5511c3 % proxies.length];
   if (useProxy && _0x1696ed) {
-    console.log(chalk.whiteBright("Proxy       : " + _0x1696ed));
+    console.log(chalk.whiteBright("Proxies     : " + _0x1696ed));
   } else {
-    console.log(chalk.whiteBright("Proxy       : Not using proxy"));
+    console.log(chalk.whiteBright("Proxies     : Not using proxy"));
   }
   console.log(chalk.cyan(_0x5866b8));
-  console.log("\nStatus:");
+  console.log("\nStatus   :");
   if (messages[_0x5511c3].startsWith('Error:')) {
     console.log(chalk.red("Account " + (_0x5511c3 + 0x1) + ": " + messages[_0x5511c3]));
   } else {
